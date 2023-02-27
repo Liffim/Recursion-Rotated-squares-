a63c2f9db5799eeec5acb5b477d529190a73e914
Liffim <karolis.seimys@gmail.com>
2023-02-27 18:47:52 +0200

    Updated, need to fix x's and y's

---
+++ ConsoleApp7/Drawing.cs
@@ -0,0 +1,123 @@
+﻿
+namespace ConsoleApp7
+{
+    internal class Drawing
+    {
+        public Drawing(string OutputName, uint Width, uint Height, uint FillingColor) // Color format is ARGB (to define recomended hex: 0xAARRGGBB), coordinates start from bottom left corner, 1 unit is 1 pixel, only 256 colors can be used
+        {
+            this.Width = Width;
+            this.Height = Height;
+            this.Buffer = new byte[Width * Height];
+            this.Colors = new List<uint>();
+
+            this.Colors.Add(FillingColor);
+
+            Array.Fill<byte>(this.Buffer, 0);
+
+            this.OutputName = OutputName;
+            if (!OutputName.Contains(".bmp"))
+                this.OutputName += ".bmp";
+        }
+
+
+        public void DrawRotatedFilledSquare(double X, double Y, double Width, double Height, uint Color = 0)
+        {
+            // Define center point of square
+            double Cx = X + Width / 2;
+            double Cy = Y + Height / 2;
+
+            // Rotate each pixel in square by 45 degrees and set color
+            for (double SY = 0; SY < Height; SY++)
+            {
+                for (double SX = 0; SX < Width; SX++)
+                {
+                    // Calculate new pixel location after rotation
+                    double Xp = SX - Width / 2;
+                    double Yp = SY - Height / 2;
+                    double Xnew = Xp * Math.Cos(Math.PI / 4) - Yp * Math.Sin(Math.PI / 4) + Cx;
+                    double Ynew = Xp * Math.Sin(Math.PI / 4) + Yp * Math.Cos(Math.PI / 4) + Cy;
+
+                    SetPixel(Xnew, Ynew, Color);
+                    SetPixel(Xnew + 1, Ynew + 1, Color);
+                    SetPixel(Xnew, Ynew + 1, Color);
+                    SetPixel(Xnew, Ynew - 1, Color);
+                }
+            }
+        }
+        private void SetPixel(double X, double Y, uint Color)
+        {
+            int Pixel = GetPixel(X, Y);
+            if (Pixel < 0)
+                return;
+
+            int CIdx = Colors.IndexOf(Color);
+            if (CIdx < 0)
+            {
+                Colors.Add(Color);
+                CIdx = Colors.Count - 1;
+            }
+
+            Buffer[Pixel] = (byte)CIdx;
+        }
+
+        private int GetPixel(double X, double Y)
+        {
+            int Pixel = ((int)Math.Round(Y) * (int)Width) + (int)Math.Round(X);
+            if (Pixel > Buffer.Length)
+                return -1;
+
+            if (X < 0)
+                return -1;
+            else if (X > Width)
+                return -1;
+
+            return Pixel;
+        }
+
+        public void Write()
+        {
+            using (FileStream File = new FileStream(OutputName, FileMode.Create, FileAccess.Write))
+            {
+                File.Write(new byte[] { 0x42, 0x4D }); // BM
+                File.Write(BitConverter.GetBytes(Height * Width * sizeof(byte) + sizeof(uint) * 255 + 0x36)); // Size
+                File.Write(BitConverter.GetBytes(0)); // Reserved (0s)
+                File.Write(BitConverter.GetBytes(0x1A)); // Image Offset (size of the header)
+
+                File.Write(BitConverter.GetBytes(40)); // Header size (size is 40 bytes)
+                File.Write(BitConverter.GetBytes(Width)); // Width
+                File.Write(BitConverter.GetBytes(Height)); // Height
+                File.Write(BitConverter.GetBytes((ushort)1)); // Color plane
+                File.Write(BitConverter.GetBytes((ushort)8)); // bits per pixel
+                File.Write(BitConverter.GetBytes((uint)0)); // Compression
+                File.Write(BitConverter.GetBytes((uint)0)); // Image size (set 0 due to compression)
+                File.Write(BitConverter.GetBytes((uint)0)); // Horizontal pixels per meter
+                File.Write(BitConverter.GetBytes((uint)0)); // Vertical pixels per meter
+                File.Write(BitConverter.GetBytes((uint)0)); // Used colors (0 all colors used)
+                File.Write(BitConverter.GetBytes((uint)0)); // Important Colors (0 all colors are important)
+
+                byte[] Pallete = new byte[256 * sizeof(uint)];
+
+                int RunColor = 0;
+                foreach (uint Color in Colors)
+                {
+                    Pallete[RunColor + 0] = (byte)((Color >> 16) & 0xFF);
+                    Pallete[RunColor + 1] = (byte)((Color >> 8) & 0xFF);
+                    Pallete[RunColor + 2] = (byte)((Color >> 0) & 0xFF);
+                    Pallete[RunColor + 3] = 0;
+
+                    RunColor += 4;
+                }
+
+                File.Write(Pallete);
+                File.Write(Buffer);
+                File.Close();
+            }
+        }
+
+        private readonly uint Width;
+        private readonly uint Height;
+        private readonly byte[] Buffer;
+        private readonly List<uint> Colors;
+        private readonly string OutputName;
+    }
+}
--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -3,76 +3,60 @@ using System.Drawing;
 
 
 
-namespace SimpleREngine
+namespace ConsoleApp7
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Renderer Render = new Renderer("Result", 1000, 1000, 0xFF808080);
-
-            // Render.DrawCircle(500, 600, 80, 0.5, 0);
-            //Render.DrawFilledCircle(100, 100, 50, 0.5, 0xFFFF0000);
-            // Render.DrawFilledSquare(100, 100, 100, 100, 0xFFFF0000);
-            //Render.DrawRotatedFilledSquare(500, 500, 100, 100, 0xFFFF0000);
-            //Render.DrawSquare(100, 100, 500, 500, 0xFFFF0000);
-
-            //Recursive(Render, 500, 500, 50, 0, naujas);
-            //Recursive(Render, 500, 500, 50, 2, naujas);
-            //Recursive(Render, 500, 500, 100, 3, naujas);
-            //RecursiveSquare(Render, 500, 500, 500, 1);
-            DrawRotatedSquare(Render, 400, 400, 200, 200, 0, 0);
-            DrawSurroundingSquares(Render, 500, 500, 100,100 ,60, 45, 3);
+            Drawing Render = new Drawing("Result", 1000, 1000, 0xFFFFFFFF);
+            DrawRotatedSquare(Render, 400, 400, 200, 200, 0);
+            DrawSurroundingSquares(Render, 500, 500, 100,100 ,75, 3);
             Render.Write();
         }
       
-        private static void DrawRotatedSquare(Renderer Render, int x, int y,int Width, int Height, int size, float angle)
+        private static void DrawRotatedSquare(Drawing Render, int x, int y,int Width, int Height, int size)
         {
-            // Calculate the points of the rotated square
-            PointF[] points = new PointF[4];
-            //points[0] = RotatePoint(new PointF(x - size / 2, y - size / 2), new PointF(x, y), angle);
-            //points[1] = RotatePoint(new PointF(x + size / 2, y - size / 2), new PointF(x, y), angle);
-            //points[2] = RotatePoint(new PointF(x + size / 2, y + size / 2), new PointF(x, y), angle);
-           // points[3] = RotatePoint(new PointF(x - size / 2, y + size / 2), new PointF(x, y), angle);
-
             // Draw the rotated square
-            Render.DrawRotatedFilledSquare((x - size / 2), (y - size / 2), Width, Height);
+            Render.DrawRotatedFilledSquare((x - size / 2), (y - size / 2), Width, Height);//c1 | n^2
         }
+        //T(n) = n^2
 
-        private static void DrawSurroundingSquares(Renderer Render, int x, int y,  int Width, int Height, int size, float angle, int depth)
+        private static void DrawSurroundingSquares(Drawing Render, int x, int y,  int Width, int Height, int size, int depth)
         {
             if (depth == 0)
             {
                 return;
-            }
+            }//                                         c1,c2 | 1,1
 
             // Calculate the coordinates of the smaller squares
-            float sqrt2 = (float)Math.Sqrt(2);
-            int dist = (int)(size * sqrt2);
-            int x1 = (int)(x - dist * 4);
-            int y1 = (int)(y - dist * 2);
-            int x2 = (int)(x + dist * 4);
-            int y2 = (int)(y - dist * 2);
-            int x3 = (int)(x);
-            int y3 = (int)(y - dist * 4.5);
-            int x4 = (int)(x - dist * 3);
-            int y4 = (int)(y + dist * 3);
-            int x5 = (int)(x + dist * 3);
-            int y5 = (int)(y + dist * 3);
+            float sqrt2 = (float)Math.Sqrt(2);//c3 | 1
+            int dist = (int)(size * sqrt2);//c4 | 1
+            int x1 = (int)(x - dist * 3);//c5 | 1
+            int y1 = (int)(y - dist * 2.5);//c6 | 1
+            int x2 = (int)(x + dist * 2);//c7 | 1
+            int y2 = (int)(y - dist * 2.5);//c8 | 1
+            int x3 = (int)(x + dist * 0.4);//c9 | 1
+            int y3 = (int)(y + dist * 3.2);//c10 | 1
+            int x4 = (int)(x - dist * 3);//c11 | 1
+            int y4 = (int)(y + dist * 1.5);//c12 | 1
+            int x5 = (int)(x + dist * 3);//c13 | 1
+            int y5 = (int)(y + dist * 1.5);//c14 | 1
+
 
             // Draw the surrounding squares rotated 45 degrees
-            DrawRotatedSquare(Render, x1, y1, Width , Height , size, angle);
-            DrawRotatedSquare(Render, x2, y2, Width , Height , size, angle);
-            DrawRotatedSquare(Render, x3, y3, Width , Height , size, angle);
-            DrawRotatedSquare(Render, x4, y4, Width , Height , size, angle);
-            DrawRotatedSquare(Render, x5, y5, Width , Height , size, angle);
+            DrawRotatedSquare(Render, x1, y1, Width , Height , size );//c15 | n^2
+            //DrawRotatedSquare(Render, x2, y2, Width , Height , size );//c16 | n^2
+            //DrawRotatedSquare(Render, x3, y3, Width , Height , size);//c17 | n^2
+            //DrawRotatedSquare(Render, x4, y4, Width , Height , size);//c18 | n^2
+            //DrawRotatedSquare(Render, x5, y5, Width , Height , size);//c19 | n^2
 
             // Recursively draw smaller squares around each of the surrounding squares
-            DrawSurroundingSquares(Render, x1, y1, Width /3, Height/3, size / 3, angle, depth - 1);
-            DrawSurroundingSquares(Render, x2, y2, Width / 3, Height / 3, size / 3, angle, depth - 1);
-            DrawSurroundingSquares(Render, x3, y3, Width / 3, Height / 3, size / 3, angle, depth - 1);
-            DrawSurroundingSquares(Render, x4, y4, Width / 3, Height / 3, size / 3, angle, depth - 1);
-            DrawSurroundingSquares(Render, x5, y5, Width / 3, Height / 3, size / 3, angle, depth - 1);
+            DrawSurroundingSquares(Render, x1, y1, Width /4, Height/4, size / 4, depth - 1);// T(
+            DrawSurroundingSquares(Render, x2, y2, Width / 4, Height / 4, size / 4, depth - 1);
+            DrawSurroundingSquares(Render, x3, y3, Width / 4, Height /4, size / 4, depth - 1);
+            DrawSurroundingSquares(Render, x4, y4, Width / 4, Height / 4, size / 4, depth - 1);
+            DrawSurroundingSquares(Render, x5, y5, Width / 4, Height / 4, size / 4, depth - 1);
 
         }
 
